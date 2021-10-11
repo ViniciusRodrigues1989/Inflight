88458afd268f4db13168007d7ecc0d85e5f558e0
ViniciusRodrigues1989 <52465206+ViniciusRodrigues1989@users.noreply.github.com>
2021-10-11 11:42:15 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,35 @@
+<?php
+$action=$_REQUEST['action'];
+if ($action=="")    /* display the contact form */
+    {
+    ?>
+    <form  action="enviar.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="action" value="submit">
+    NOME	
+    <br>
+    <input name="name" type="text" value="" size="30"/><br>
+    E-MAIL<br>
+    <input name="email" type="text" value="" size="30"/><br>
+    MENSAGEM<br>
+    <textarea name="message" rows="7" cols="30"></textarea><br>
+    <input type="submit" value="Enviar Mensagem" class="special" />
+    <input type="RESET" value="LIMPAR"/>
+    </form><?php
+    } 
+else                /* send the submitted data */
+    {
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("atendimento@inflightfotoevideo.com.br", $subject, $message, $from);
+        echo "E-mail Enviado Com Sucesso! Por Favor aguarde nosso retorno.";
+   }
+    }  
+?>
